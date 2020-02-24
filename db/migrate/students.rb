cfb082569ddc376cc7a77857072a166210e7349f
James Lenahan <james.k.lenahan@gmail.com>
2020-02-24 21:07:25 +0000

    Automatically backed up by Learn

--- db/migrate/students.rb
+++ db/migrate/students.rb
@@ -1,10 +1,8 @@
-class Students < ActiveRecord::Migration
+class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
- 
-      t.timestamps null: false
     end
   end
 end
